949e3cb4d9b79b15a420bf0109c02833d9d9f24e
John S. Urban <urbanjost@comcast.net>
2021-09-26 10:48:49 -0400

    example indenting for doxygen

--- src/M_hashkeys.f90
+++ src/M_hashkeys.f90
@@ -423,7 +423,7 @@ integer(kind=c_int32_t),parameter :: k0_ref(64)=[ &
 
    break  = 0
    pos0   = 1
-   length = len_trim(str)
+   length = len(trim(str))
 
    do while (break .ne. 1)
 
@@ -1363,33 +1363,37 @@ end subroutine test_suite_sha256
 !!
 !!  Sample program
 !!
-!!    program demo_luhn_checksum
-!!    use M_hashkeys, only : luhn_checksum
-!!    implicit none
-!!    character(len=:),allocatable :: ccards(:), string
-!!    integer :: i, j
-!!    write(*,*)'GOOD VALUES'
-!!    ccards=[ character(len=20) :: '79927398713', '49927398716', '1234567812345670' ]
-!!    call checkem()
-!!    write(*,*)'BAD VALUES'
-!!    ccards=[ character(len=20) :: &
-!!       '79927398710','79927398711','79927398712','79927398714', &
-!!       '79927398715','79927398716','79927398717','79927398718','79927398719', &
-!!       '49927398717', '1234567812345678' ]
-!!    call checkem()
-!!    contains
-!!    subroutine checkem
-!!       ! validate these numbers
-!!       do i=1,size(ccards)
-!!          j=len(trim(ccards(i)))
-!!          string=luhn_checksum(ccards(i)(:j-1))
-!!          write(*,'(a,1x,a,1x,l1)')ccards(i),string,ccards(i).eq.string
-!!       enddo
+!!      program demo_luhn_checksum
+!!      use M_hashkeys, only : luhn_checksum
+!!      implicit none
+!!      character(len=:),allocatable :: ccards(:), string
+!!      integer :: i, j
+!!      write(*,*)'GOOD VALUES'
+!!      ccards=[ character(len=20) :: '79927398713', &
+!!                                  & '49927398716',&
+!!                                  & '1234567812345670' ]
+!!      call checkem()
+!!      write(*,*)'BAD VALUES'
+!!      ccards=[ character(len=20) :: &
+!!        & '79927398710', '79927398711', '79927398712', '79927398714',  &
+!!        & '79927398715', '79927398716', '79927398717', '79927398718',  &
+!!        & '79927398719',  &
+!!         '49927398717', '1234567812345678' ]
+!!      call checkem()
+!!      contains
+!!      subroutine checkem
+!!         ! validate these numbers
+!!         do i=1,size(ccards)
+!!            j=len(trim(ccards(i)))
+!!            string=luhn_checksum(ccards(i)(:j-1))
+!!            write(*,'(a,1x,a,1x,l1)')ccards(i),string,ccards(i).eq.string
+!!         enddo
 !!
-!!       string='123456 781-234-567'
-!!       write(*,*)'from ',string,' got ',luhn_checksum(string),' which should be 1234567812345670'
-!!    end subroutine checkem
-!!    end program demo_luhn_checksum
+!!         string='123456 781-234-567'
+!!         write(*,*)'from ',string,' got ',luhn_checksum(string), &
+!!         & ' which should be 1234567812345670'
+!!      end subroutine checkem
+!!      end program demo_luhn_checksum
 !===================================================================================================================================
 !()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()()=
 !===================================================================================================================================
@@ -1407,7 +1411,7 @@ integer                      :: value
 integer                      :: d2
 integer                      :: ios
   string_local=transliterate(string,' -','')     ! delete spaces and dashes
-  n = len_trim(string_local)                     ! Count the digits in string_local assuming the string_local is all digits.
+  n = len(trim(string_local))                    ! Count the digits in string_local assuming the string_local is all digits.
   allocate(dgts(n))
   read(string_local,'(*(i1))',iostat=ios)(dgts(i),i=1,n) ! Extract the digits from S.
   if(ios.ne.0)then
